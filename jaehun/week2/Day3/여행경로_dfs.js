abcb6112c5110d0191556af80a170bdb2713a474
hun2__2 <wognskec@hanyang.ac.kr>
2023-09-27 17:24:07 +0900

    [여행경로 문제] 내풀이, 강사님 풀이

---
+++ jaehun/week2/Day3/여행경로_dfs.js
@@ -0,0 +1,59 @@
+function solution(tickets) {
+    const grpah = {};
+    for (const [a, b] of tickets) {
+        grpah[a] = grpah[a] ? [...grpah[a], b] : [b];
+        grpah[b] = grpah[b] ? grpah[b] : [];
+    }
+    // console.log(grpah);
+    Object.keys(grpah).forEach((key) => grpah[key].sort());
+    // console.log(grpah);
+
+    let list = ["ICN"];
+    (function dfs(cur) {
+        for (let i = 0; i < grpah[cur].length; i++) {
+            // console.log("grpah[cur][i]", grpah[cur][i]);
+            const next = grpah[cur][i];
+            if (next) {
+                list.push(next);
+                grpah[cur][i] = null;
+                dfs(next);
+                // 모두 방문 못했으면 다른 경로
+                if (list.length !== tickets.length + 1) {
+                    grpah[cur][i] = next;
+                    list.pop();
+                }
+                // console.log("list: ", list);
+                // console.log("grpah: ", grpah);
+            }
+        }
+    })("ICN");
+    return list;
+}
+
+console.log(
+    solution([
+        ["ICN", "AAA"],
+        ["ICN", "CCC"],
+        ["CCC", "DDD"],
+        ["AAA", "BBB"],
+        ["AAA", "BBB"],
+        ["DDD", "ICN"],
+        ["BBB", "AAA"],
+    ])
+);
+console.log(
+    solution([
+        ["ICN", "JFK"],
+        ["HND", "IAD"],
+        ["JFK", "HND"],
+    ])
+);
+console.log(
+    solution([
+        ["ICN", "SFO"],
+        ["ICN", "ATL"],
+        ["SFO", "ATL"],
+        ["ATL", "ICN"],
+        ["ATL", "SFO"],
+    ])
+);
