2bfab6e7f1e1013319a8cb0f730cd4ca338cf0c6
lunarmoon7 <49crehbgr@gmail.com>
2023-09-27 17:31:26 +0900

    add: 노드 제거 함수 추가

--- hwisik/week2/Day2/binarySearchTree.js
+++ hwisik/week2/Day2/binarySearchTree.js
@@ -38,6 +38,51 @@ class BinarySearchTree {
     }
   }
 
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    } else if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      // case 1: no children
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      // case 2: one child
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      // case 3: two children
+      let minRight = this.findMinNode(node.right);
+      node.value = minRight.value;
+      node.right = this.removeNode(node.right, minRight.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
   has(value) {
     let currNode = this.root;
     while(currNode !== null) {
@@ -55,3 +100,32 @@ class BinarySearchTree {
     return false;
   }
 }
+
+const bst = new BinarySearchTree();
+bst.insert(5);
+bst.insert(4);
+bst.insert(7);
+bst.insert(2);
+bst.insert(5);
+bst.insert(8);
+bst.insert(3);
+
+// Before Remove
+console.log('-----------Before Remove-----------\n',bst.root.value); // 5
+console.log(bst.root.left.value); // 4
+console.log(bst.root.right.value); // 7
+console.log(bst.root.left.left.value); // 2
+console.log(bst.root.left.right.value); // 5
+console.log(bst.root.right.right.value); // 8
+console.log(bst.root.left.left.right.value); // 3
+console.log('----------------------');
+
+// After Remove
+bst.remove(4);
+
+console.log('-----------After Remove-----------\n',bst.root.value); // 5
+console.log(bst.root.left.value); // 5
+console.log(bst.root.right.value); // 7
+console.log(bst.root.left.left.value); // 2
+console.log(bst.root.left.left.right.value); // 3
+console.log(bst.root.right.right.value); // 8
