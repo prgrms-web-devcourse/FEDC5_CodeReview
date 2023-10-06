8ec729fe0583464937b0e7045b4cbeeaeb875679
lunarmoon7 <49crehbgr@gmail.com>
2023-10-06 17:25:27 +0900

    bug: new TodoItem()에서 버그 발생 - 태그가 추가되는게 아닌, [object Object]가 문자열로 가됨

---
+++ hwisik/week3/Day5/src/TodoList.js
@@ -0,0 +1,30 @@
+// params.$target - 해당 컴포넌트가 추가가 될 DOM 엘리먼트
+// params.$initialState - 해당 컴포넌트의 초기 상태값
+// $ : DOM 객체를 담고있는 변수를 의미
+function TodoList(params) {
+  // new 키워드를 사용하지 않았을 경우 예외 처리
+  if (!(this instanceof TodoList)) {
+    throw new Error('TodoList Component must be called with "new"');
+  }
+
+  const $todoList = document.createElement('div');
+  const $target = params.$target;
+
+  this.state = params.initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    console.log(new TodoItem({ $target: $todoList, initialState: { text: 'test', isCompleted: false }}))
+    $todoList.innerHTML = `
+    <ul>
+      ${new TodoItem({ $target: $todoList, initialState: { text: 'test', isCompleted: false }})}
+    </ul>
+    `;
+    $target.appendChild($todoList);
+  };
+  this.render();
+}
